a9a77f75259a87b7830aa63e5e977794a4e9ae2c
Georgeos Hardo <gh464@cantab.ac.uk>
2025-07-17 21:11:54 -0400

    Remove widget boilerplate

--- src/napari_geff/__init__.py
+++ src/napari_geff/__init__.py
@@ -5,15 +5,10 @@ except ImportError:
 
 
 from ._reader import napari_get_reader
-from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
 from ._writer import write_multiple, write_single_image
 
 __all__ = (
     "napari_get_reader",
     "write_single_image",
     "write_multiple",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
 )
