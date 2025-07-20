90d530ad99b6b8448d3b4415e68a9a70380cf17f
Georgeos Hardo <gh464@cantab.ac.uk>
2025-07-20 17:20:44 -0400

    Bring utils into _writer and implement minimal writer

--- src/napari_geff/_writer.py
+++ src/napari_geff/_writer.py
@@ -12,55 +12,147 @@ from __future__ import annotations
 from collections.abc import Sequence
 from typing import TYPE_CHECKING, Any, Union
 
+import networkx as nx
+import pandas as pd
+from geff import write_nx
+
 if TYPE_CHECKING:
     DataType = Union[Any, Sequence[Any]]
     FullLayerData = tuple[DataType, dict, str]
 
 
-def write_single_image(path: str, data: Any, meta: dict) -> list[str]:
-    """Writes a single image layer.
+def write_tracks(path: str, data: Any, meta: dict) -> list[str]:
+    layer_metadata = meta["metadata"]
+    features = meta["features"]
+    tracklets_graph = meta["graph"]
 
-    Parameters
-    ----------
-    path : str
-        A string path indicating where to save the image file.
-    data : The layer data
-        The `.data` attribute from the napari layer.
-    meta : dict
-        A dictionary containing all other attributes from the napari layer
-        (excluding the `.data` layer attribute).
+    if "geff_metadata" in layer_metadata:
+        axis_names = [
+            axis.name for axis in layer_metadata["geff_metadata"].axes
+        ]
+        axis_types = [
+            axis.type for axis in layer_metadata["geff_metadata"].axes
+        ]
+        tracks_layer_df = features
+        assert (
+            "node_id" in tracks_layer_df.columns
+        ), "Tracks layer must have a node_id column"
+    else:
+        axis_names = list(meta["axis_labels"])
+        axis_types = ["time"] + ["space"] * (len(axis_names) - 1)
+        tracks_layer_df = pd.DataFrame(
+            data,
+            columns=["napari_track_id"] + axis_names,
+            # napari assumes the first two cols are tid and t
+        )
+        # Since no metadata, use index as node id. TODO maybe allow user to specify a node id in features or metadata
+        tracks_layer_df["node_id"] = tracks_layer_df.index
 
-    Returns
-    -------
-    [path] : A list containing the string path to the saved file.
-    """
+    edge_df = get_edge_df(
+        tracks_layer_data=tracks_layer_df,
+        tracklets_graph=tracklets_graph,
+        axis_names=axis_names,
+        axis_types=axis_types,
+    )
 
-    # implement your writer logic here ...
+    nx_graph = create_nx_graph(
+        tracks_layer_data=tracks_layer_df,
+        edges_df=edge_df,
+        axis_names=axis_names,
+        axis_types=axis_types,
+    )
 
-    # return path to any file(s) that were successfully written
+    write_nx(
+        nx_graph,
+        path,
+        axis_names=axis_names,
+        axis_types=axis_types,
+    )
+
     return [path]
 
 
-def write_multiple(path: str, data: list[FullLayerData]) -> list[str]:
-    """Writes multiple layers of different types.
+def get_edge_df(
+    tracks_layer_data: pd.DataFrame,
+    tracklets_graph: dict[int, list[int]],
+    axis_names: list[str],
+    axis_types: list[str],
+) -> pd.DataFrame:
 
-    Parameters
-    ----------
-    path : str
-        A string path indicating where to save the data file(s).
-    data : A list of layer tuples.
-        Tuples contain three elements: (data, meta, layer_type)
-        `data` is the layer data
-        `meta` is a dictionary containing all other metadata attributes
-        from the napari layer (excluding the `.data` layer attribute).
-        `layer_type` is a string, eg: "image", "labels", "surface", etc.
+    edges = []
 
-    Returns
-    -------
-    [path] : A list containing (potentially multiple) string paths to the saved file(s).
+    # Get the name of the time axis
+    t_axis = axis_names[axis_types.index("time")]
+
+    tracks_layer_data.sort_values(by=["napari_track_id", t_axis], inplace=True)
+
+    # First do the intra-tracklet edges
+    for _tid, track_df in tracks_layer_data.groupby("napari_track_id"):
+
+        nodes = track_df["node_id"].tolist()
+
+        # add source and target nodes
+        for node1, node2 in zip(nodes[:-1], nodes[1:], strict=False):
+            edges.append({"source": node1, "target": node2})
+
+    # Next get the splits and merges from the graph dictionary
+    if tracklets_graph:
+
+        tracklet_extrema = {
+            tid: {
+                "first": tracklet_df["node_id"].iloc[
+                    0
+                ],  # it works because they're sorteed by time
+                "last": tracklet_df["node_id"].iloc[-1],
+            }
+            for tid, tracklet_df in tracks_layer_data.groupby(
+                "napari_track_id"
+            )
+        }
+
+        for (
+            daughter_tracklet_id,
+            parent_tracklet_ids,
+        ) in tracklets_graph.items():
+            for parent_tracklet_id in parent_tracklet_ids:
+                edges.append(
+                    {
+                        "source": tracklet_extrema[parent_tracklet_id]["last"],
+                        "target": tracklet_extrema[daughter_tracklet_id][
+                            "first"
+                        ],
+                    }
+                )
+
+    return pd.DataFrame(edges)
+
+
+def create_nx_graph(
+    tracks_layer_data: pd.DataFrame,
+    edges_df: pd.DataFrame,
+    axis_names: list[str],
+    axis_types: list[str],
+) -> nx.DiGraph:
     """
+    Create a networkx directed graph from a napari Tracks layer.
+    """
+    nx_graph = nx.from_pandas_edgelist(edges_df, create_using=nx.DiGraph())
 
-    # implement your writer logic here ...
+    node_axis_properties = (
+        tracks_layer_data.loc[:, axis_names + ["node_id"]]
+        .set_index("node_id")
+        .to_dict(orient="index")
+    )
+    nx.set_node_attributes(
+        nx_graph, node_axis_properties
+    )  # TODO edge attributes and other node attributes
 
-    # return path to any file(s) that were successfully written
-    return [path]
+    return nx_graph
+
+
+def write_multiple():
+    pass
+
+
+def write_single_image():
+    pass
