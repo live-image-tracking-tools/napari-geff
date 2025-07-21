ee43d618c02fe797316a559a5a4f2e3c52bf2619
Georgeos Hardo <gh464@cantab.ac.uk>
2025-07-21 18:21:45 -0400

    graph diff tool

---
+++ src/napari_geff/utils.py
@@ -0,0 +1,116 @@
+def compare_graphs(g1, g2):
+    """
+    Compares two NetworkX graphs and returns a detailed list of differences,
+    including strict checks for attribute values and data types.
+
+    Args:
+        g1 (networkx.Graph): The first graph to compare.
+        g2 (networkx.Graph): The second graph to compare.
+
+    Returns:
+        list: A list of tuples describing the differences. An empty list means
+              the graphs are identical. The output format for attribute
+              differences is now more granular.
+              - ('node_attribute_diff', node, key, (val1, type1), (val2, type2))
+              - ('edge_attribute_diff', edge, key, (val1, type1), (val2, type2))
+    """
+    diffs = []
+
+    # Check for differences in graph type
+    is_directed_mismatch = g1.is_directed() != g2.is_directed()
+    is_multi_mismatch = g1.is_multigraph() != g2.is_multigraph()
+    if is_directed_mismatch or is_multi_mismatch:
+        type1 = f"directed={g1.is_directed()}, multigraph={g1.is_multigraph()}"
+        type2 = f"directed={g2.is_directed()}, multigraph={g2.is_multigraph()}"
+        diffs.append(("graph_type", type1, type2))
+        return diffs
+
+    # Compare nodes
+    nodes1, nodes2 = set(g1.nodes), set(g2.nodes)
+    for node in nodes1 - nodes2:
+        diffs.append(("node_missing_from_g2", node))
+    for node in nodes2 - nodes1:
+        diffs.append(("node_missing_from_g1", node))
+
+    # More granular node attribute comparison (value and type)
+    for node in nodes1 & nodes2:
+        attrs1, attrs2 = g1.nodes[node], g2.nodes[node]
+        all_keys = set(attrs1.keys()) | set(attrs2.keys())
+        for key in all_keys:
+            val1, val2 = attrs1.get(key), attrs2.get(key)
+            # Check for difference in value OR type
+            if val1 != val2 or type(val1) != type(val2):
+                type1_str = type(val1).__name__ if key in attrs1 else "N/A"
+                type2_str = type(val2).__name__ if key in attrs2 else "N/A"
+                v1_rep = val1 if key in attrs1 else "<missing>"
+                v2_rep = val2 if key in attrs2 else "<missing>"
+                diffs.append(
+                    (
+                        "node_attribute_diff",
+                        node,
+                        key,
+                        (v1_rep, type1_str),
+                        (v2_rep, type2_str),
+                    )
+                )
+
+    # Compare edges
+    if g1.is_multigraph():
+        edges1, edges2 = set(g1.edges(keys=True)), set(g2.edges(keys=True))
+    else:
+        edges1, edges2 = set(g1.edges()), set(g2.edges())
+
+    if not g1.is_directed():
+
+        def canonical_edge(edge):
+            u, v, *key = edge
+            if u > v:
+                u, v = v, u
+            return (u, v, *key)
+
+        map1, map2 = {canonical_edge(e): e for e in edges1}, {
+            canonical_edge(e): e for e in edges2
+        }
+        canon_edges1, canon_edges2 = set(map1.keys()), set(map2.keys())
+    else:
+        map1, map2 = {e: e for e in edges1}, {e: e for e in edges2}
+        canon_edges1, canon_edges2 = edges1, edges2
+
+    for edge in canon_edges1 - canon_edges2:
+        diffs.append(("edge_missing_from_g2", map1[edge]))
+    for edge in canon_edges2 - canon_edges1:
+        diffs.append(("edge_missing_from_g1", map2[edge]))
+
+    # Granular edge attribute comparison (value and type)
+    for edge in canon_edges1 & canon_edges2:
+        orig_edge1, orig_edge2 = map1[edge], map2[edge]
+        attrs1 = (
+            g1.get_edge_data(*orig_edge1)
+            if g1.is_multigraph()
+            else g1.get_edge_data(orig_edge1[0], orig_edge1[1])
+        )
+        attrs2 = (
+            g2.get_edge_data(*orig_edge2)
+            if g2.is_multigraph()
+            else g2.get_edge_data(orig_edge2[0], orig_edge2[1])
+        )
+
+        all_keys = set(attrs1.keys()) | set(attrs2.keys())
+        for key in all_keys:
+            val1, val2 = attrs1.get(key), attrs2.get(key)
+            # Check for difference in value OR type
+            if val1 != val2 or type(val1) != type(val2):
+                type1_str = type(val1).__name__ if key in attrs1 else "N/A"
+                type2_str = type(val2).__name__ if key in attrs2 else "N/A"
+                v1_rep = val1 if key in attrs1 else "<missing>"
+                v2_rep = val2 if key in attrs2 else "<missing>"
+                diffs.append(
+                    (
+                        "edge_attribute_diff",
+                        edge,
+                        key,
+                        (v1_rep, type1_str),
+                        (v2_rep, type2_str),
+                    )
+                )
+    return diffs
