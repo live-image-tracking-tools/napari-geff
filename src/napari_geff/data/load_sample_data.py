37210b062a8cf47063c8f3c23573fbac6575f4d7
Georgeos Hardo <gh464@cantab.ac.uk>
2025-07-23 15:20:46 -0400

    Load sample data

---
+++ src/napari_geff/data/load_sample_data.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from napari_geff._reader import reader_function
+
+
+def load_sample_data():
+    script_path = Path(__file__).resolve()
+    path = (
+        script_path.parent.parent.parent.parent
+        / "examples/graph_labels_seg.zarr/tracks"
+    )
+    path = str(path)
+    layers = reader_function(path)
+    return layers
