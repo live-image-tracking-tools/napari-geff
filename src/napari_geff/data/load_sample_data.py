9f65882681889622cfab4dd2ef6f14d83e9422df
Georgeos Hardo <gh464@cantab.ac.uk>
2025-07-23 15:21:41 -0400

    Move examples

--- src/napari_geff/data/load_sample_data.py
+++ src/napari_geff/data/load_sample_data.py
@@ -6,7 +6,7 @@ from napari_geff._reader import reader_function
 def load_sample_data():
     script_path = Path(__file__).resolve()
     path = (
-        script_path.parent.parent.parent.parent
+        script_path.parent.parent.parent
         / "examples/graph_labels_seg.zarr/tracks"
     )
     path = str(path)
